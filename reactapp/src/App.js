3ad671a41477788fb864110c326a4db307037c41
RiderPy <utkarshgoelksp123@gmail.com>
2023-06-05 10:06:39 +0000

    added the lesson page for student dashboard

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CourseDetails from './pages/instructordashboard/CourseDetails';
 import Dashboard from './pages/instructordashboard/Dashboard';
+import LessonPage from './pages/lessonPages/LessonPage';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/instructor" element={<Dashboard />}/>
         <Route path="/instructor/courses/:courseId" element={<CourseDetails />} />
+        <Route path='lessonpage' element={<LessonPage />} />
       </Routes>
     </BrowserRouter>
   );
