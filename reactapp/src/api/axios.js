8f8172639e1857b4a12c1c41691df1dbf843b691
RajithaHyma <vemularajithahyma@gmail.com>
2023-07-03 12:40:58 +0000

    changesdone

--- reactapp/src/api/axios.js
+++ reactapp/src/api/axios.js
@@ -1,2 +1,2 @@
-const BASE_URL = "https://8080-abdcffedaacedadccddafbcdeaeaadbdbabf.project.examly.io/";
+const BASE_URL="https://8080-ceddffcfcefcbcdadccddafbcddcbcabfdfafdade.project.examly.io/";
 export default BASE_URL;
