fd8d9c5e4696e051575680a3101f1f85ae0fab62
chinnamramanjaneyulu57 <chinnamramanjaneyulu57@gmail.com>
2023-07-11 11:38:05 +0000

    Course Catalogue

--- reactapp/src/components/LandHome.js
+++ reactapp/src/components/LandHome.js
@@ -7,7 +7,7 @@ function LandHome(props){
             <div className="hero-text">
                 <h1>{props.title}</h1>
                 <p>{props.text}</p>
-                <a href="/catalogue" className={props.btnClass}>
+                <a href={props.url} className={props.btnClass}>
                     {props.buttonText}
                 </a>
             </div>
