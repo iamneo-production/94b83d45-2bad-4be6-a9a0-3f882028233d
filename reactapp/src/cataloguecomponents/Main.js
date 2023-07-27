b3bab5fd17562df5be82f3db8c5194a0e725322f
chinnamramanjaneyulu57 <chinnamramanjaneyulu57@gmail.com>
2023-07-27 09:22:22 +0000

    Course Catalogue

--- reactapp/src/cataloguecomponents/Main.js
+++ reactapp/src/cataloguecomponents/Main.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from "react";
+/*import React, { useState , useEffect} from "react";
 import { Courses } from './Card';
 // import contents from './content';
 import "./cataloguestyle.css";
@@ -11,7 +11,8 @@ useEffect(() => {
       try {
         const response = await axios.get(`${BASE_URL}course`);
         setContent(response.data);
-      } catch (error) {
+      } 
+      catch (error) {
         console.error(error);
       }
     };
@@ -44,7 +45,7 @@ useEffect(() => {
                 </div>
             </div>
             <div className='cataloguecoursecontainer'>
-            {/* contents.filter(content=>(content.title.toLowerCase().includes(query.toLowerCase()) || content.description.toLowerCase().includes(query.toLowerCase()))) */}
+            {/* contents.filter(content=>(content.title.toLowerCase().includes(query.toLowerCase()) || content.description.toLowerCase().includes(query.toLowerCase()))) }
                 
                 {contents.filter(content=>(content.title.toLowerCase().includes(query.toLowerCase()) || content.description.toLowerCase().includes(query.toLowerCase())))
                 .map((contents) => (
@@ -55,6 +56,59 @@ useEffect(() => {
                         //instructor={contents.instructor}
                         price={contents.price}
                         description={contents.description}
+                    
+                    />
+                 ))}
+            </div>
+            
+        </>
+    )
+}
+export default Main;*/
+
+
+
+import React, { useState } from "react";
+import { Courses } from './Card';
+import "./cataloguestyle.css";
+import coursesData from './content';
+const Main=()=>{
+    const [query,setQuery]  =useState("");
+    return(
+        <>
+            <div className="catalogueheader">
+                <div className="cataloguerow1">
+                    <h1>Explore  All  Courses  Here</h1>
+                </div>
+                <div className="cataloguerow2">
+                    <h2>Find Your Course</h2>
+                    <div className="cataloguesearch">
+                        <input type ="text" placeholder ="Search...." 
+                        onChange={(e)=> setQuery(e.target.value)}
+                        />
+                        <button>
+                            <i className="fas fa-search"></i>
+                        </button>
+                    </div>
+                    
+                </div>
+                <div className='back'>
+                    <a href="/">
+                        <button>Back to Home</button>
+                    </a>
+                </div>
+            </div>
+            <div className='cataloguecoursecontainer'>
+            {coursesData.filter(content=>(content.name.toLowerCase().includes(query.toLowerCase()) || content.instructor.toLowerCase().includes(query.toLowerCase()) || content.description.toLowerCase().includes(query.toLowerCase())))
+                .map((conten) => (
+                    <Courses
+                        key={conten.id}
+                        name={conten.name}
+                        //image={conten.image}
+                        //instructor={conten.instructor}
+                        price={conten.price}
+                        //rating={conten.rating}
+                        description={conten.description}
                     
                     />
                  ))}
