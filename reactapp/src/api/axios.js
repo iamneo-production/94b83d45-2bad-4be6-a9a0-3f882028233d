5ff5007809b73fd4daf8e2ab750b437fc6d372bb
sravanthchilukuri <sravanth2212@gmail.com>
2023-07-31 10:42:33 +0000

    commit

--- reactapp/src/api/axios.js
+++ reactapp/src/api/axios.js
@@ -1,2 +1,2 @@
-const BASE_URL = "https://8080-abdcffedaacedadccddafbcddcbcabfdfafdade.project.examly.io/";
+const BASE_URL = "https://8080-abdcffedaacedadccddafbcdeaeaadbdbabf.project.examly.io/";
 export default BASE_URL;
